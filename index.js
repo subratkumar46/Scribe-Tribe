da9d005d9ab74a7cf59c4da6eef3117d05a3d3af
Subrat <subrat3471@gmail.com>
2023-02-28 22:13:17 +0530

    Improved exam section of students

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('./src/db/connection');
 const path = require('path');
 const dotenv = require('dotenv');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+
+const userRoutes = require('./src/routes/user');
+const examRoutes = require('./src/routes/exam');
 
 const app = express();
 dotenv.config();
@@ -14,6 +18,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
 app.use(cookieParser());
+app.use('/', userRoutes);
+app.use('/', examRoutes);
+
 app.listen(port, () => {
     console.log(`connection is live at port ${port}`);
 });
